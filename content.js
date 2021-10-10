c80bb8099c2716349a60cb97c969f870fadf9aea
Milana <milka3341@gmail.com>
2021-10-10 18:31:19 +0600

    Update content.js

--- content.js
+++
@@ -1,8 +0,0 @@
-var inputs = document.querySelectorAll('input')
-console.log(inputs)
-if (inputs !== null) {
-    for (const item of inputs){ 
-        $(item).attr('x-webkit-speech',true);
-    }
-}
-
