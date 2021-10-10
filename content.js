fcfd62760c8eab361931293d17ba16f85f556355
Milana <milka3341@gmail.com>
2021-10-10 21:29:57 +0600

    bup

--- content.js
+++ content.js
@@ -1,6 +1,26 @@
-const blockTitle = document.querySelector('.block-title')
-const blockText = document.querySelector('.block-text')
-
-blockTitle.addEventListener('click', function() {
-    blockText.classList.toggle('active')
-})
+document.addEventListener("DOMContentLoaded", function () {
+    var buttonDark = document.getElementById("dark");
+    var buttonSepia = document.getElementById("sepia");
+    buttonDark.addEventListener("click", function () {
+      document.body.style.backgroundColor = "red";
+    });
+    buttonSepia.addEventListener("click", function () {
+      document.body.style.backgroundColor = "white";
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    var textOptionsTitle1 = document.querySelector("#block-title1");
+    var blockText2 = document.querySelector("#block-text1");
+    textOptionsTitle1.addEventListener("click", function () {
+      blockText2.classList.toggle("active");
+    });
+  });
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    var textOptionsTitle2 = document.querySelector("#block-title2");
+    var blockText2 = document.querySelector("#block-text2");
+    textOptionsTitle2.addEventListener("click", function () {
+      blockText2.classList.toggle("active");
+    });
+  });
