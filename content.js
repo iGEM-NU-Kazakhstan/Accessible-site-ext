75109cc596cdd0575921d647fc345299b54042ae
Milana <milka3341@gmail.com>
2021-10-10 23:10:13 +0600

    little changes

--- content.js
+++ content.js
@@ -1,26 +1,33 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var buttonDark = document.getElementById("dark");
-    var buttonSepia = document.getElementById("sepia");
-    buttonDark.addEventListener("click", function () {
-      document.body.style.backgroundColor = "red";
-    });
-    buttonSepia.addEventListener("click", function () {
-      document.body.style.backgroundColor = "white";
-    });
-  });
-  
+
   document.addEventListener("DOMContentLoaded", function () {
     var textOptionsTitle1 = document.querySelector("#block-title1");
-    var blockText2 = document.querySelector("#block-text1");
-    textOptionsTitle1.addEventListener("click", function () {
-      blockText2.classList.toggle("active");
-    });
-  });
-  
-  document.addEventListener("DOMContentLoaded", function () {
+    var blockText1 = document.querySelector("#block-text1");
+
     var textOptionsTitle2 = document.querySelector("#block-title2");
     var blockText2 = document.querySelector("#block-text2");
-    textOptionsTitle2.addEventListener("click", function () {
-      blockText2.classList.toggle("active");
+
+    textOptionsTitle1.addEventListener("click", function () {
+      blockText1.classList.toggle("active");
+      textOptionsTitle1.classList.toggle("active");
+      blockText2.classList.remove("active");
+      textOptionsTitle2.classList.remove("active");
     });
+    textOptionsTitle2.addEventListener("click", function () {
+        blockText1.classList.remove("active");
+        textOptionsTitle1.classList.remove("active");
+        blockText2.classList.toggle("active");
+        textOptionsTitle2.classList.toggle("active");
+      });
   });
+  
+document.addEventListener('DOMContentLoaded',function(){
+    var element = document.getElementById('dark');
+    element.addEventListener('click',function(){
+        chrome.tabs.getSelected(null, function(tab){
+            chrome.tabs.executeScript(tab.id,{file:"alert.js"}, function(response){
+                
+            })
+        })
+
+    })
+})
