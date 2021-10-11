eb1b5cefc9593a6c4ee043c7b04c5ab441a65bdb
Milana <milka3341@gmail.com>
2021-10-11 14:46:08 +0600

    Delete design.js

--- design.js
+++
@@ -1,8 +0,0 @@
-var style = document.createElement("style");
-style.className="color";
-style.innerHTML = ".select::selection{color: #fff;background-color: #000;}";
-if (document.querySelectorAll("style.color")[0] == null){
-    document.getElementsByTagName("head")[0].appendChild(style);
-}
-
-sdocument.querySelectorAll("*").forEach(e => e.classList.toggle("select"));
