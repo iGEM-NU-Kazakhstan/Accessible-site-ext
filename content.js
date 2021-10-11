eb1a99f3459787e1bf3928202d577713bfb3aa18
Milana <milka3341@gmail.com>
2021-10-11 14:45:57 +0600

    Update content.js

--- content.js
+++ content.js
@@ -283,6 +283,12 @@ document.addEventListener("DOMContentLoaded", function () {
         }else{
             checkbox.value = "Enabled"
         }
+        my_code = 'if (document.querySelectorAll("style.color")[0] == null){'+
+                        'var style = document.createElement("style");'+
+                        'style.innerHTML = ".select::selection{color: #fff;background-color: #000;}";'+
+                        'style.className="color"; '+
+                        'document.getElementsByTagName("head")[0].appendChild(style);}'+
+                        'document.querySelectorAll("*").forEach(e => e.classList.toggle("select"));'
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.executeScript(tabs[0].id, {
                 file: "design.js"
